36c607c2cbc544e176ed780f6894e2bf778a24ef
Uri Shaked <uri@salsa4fun.co.il>
2016-12-13 01:48:54 +0200

    Updates

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{360:function(n,o){},664:function(n,o,c){n.exports=c(360)}},[664]);
+webpackJsonp([1,2],{361:function(n,o){},669:function(n,o,c){n.exports=c(361)}},[669]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
